cf80e37b039787f03459de96dd37651c754589b2
valls <thiago.bertolino@zup.com.br>
2022-12-12 22:44:50 -0300

    Creating playground file to practice the swift language

---
+++ foods/foods/MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+var age:Int = 12
+var name = "Amanda Melo"
+var isAdult:Bool = true
+
+func printName(name: String) {
+    print("The name is \(name)")
+}
+
+// simple comment
+
+/*
+    multiple lines comment
+    can be add in this way
+    how many you want
+ */
+
+printName(name: name)
